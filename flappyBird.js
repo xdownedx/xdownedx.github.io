97374fc836d412cfc920f888a60c06b06bb67e01
Max Palehin <59222687+xdownedx@users.noreply.github.com>
2021-05-30 21:30:16 +0300

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -80,7 +80,7 @@ function draw(){
         // detect collision
         
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            alert("Adele prosti menya blin!");
+            alert("Adele prosti svin'yu blin! i ne obijaisya");
             location.reload(); // reload the page
         }
         
